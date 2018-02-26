1535fcd2a8ed29aa4cfa67bb0970e2df2027abf4
Gabriel Schade <re035148@ITAW10N07304>
2018-02-26 16:48:40 -0300

    Criação do projeto

---
+++ Publish.Subscribe.Exemplo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Publish.Subscribe.Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
